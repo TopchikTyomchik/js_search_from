333df9bc69fda9c3c9d9af2810274280f310c6aa
TopchikTyomchik <101976747+TopchikTyomchik@users.noreply.github.com>
2022-11-05 16:46:55 +0300

    new

--- js/script.js
+++ js/script.js
@@ -20,11 +20,14 @@ function keyup(event) {
 
 document.addEventListener('click', function (event) {
 	const form = document.querySelector('form');
+	const d1Span = document.querySelector('.d1 > span');
 	const searchIcon = event.target.closest('.search__icon');
 	if (searchIcon) {
 		form.classList.toggle("form__active");
+		d1Span.classList.toggle("none");
 	} else if (!event.target.closest("form")){
 		form.classList.remove("form__active");
+		d1Span.classList.remove("none");
 	}
 	
 	event.preventDefault();
